d96c2131eb49f1f9a3a90642ab07fca68fc990d8
Dustin Pilgrim <dustin.pilgrim1997@gmail.com>
2026-02-23 19:46:43 -0400

    Remove upstream src, keep rewrite crate folder

--- src/bin/gessod.rs
+++
@@ -1,8 +0,0 @@
-// Author: Dustin Pilgrim
-// License: MIT
-
-use anyhow::Result;
-
-fn main() -> Result<()> {
-    gesso::daemon::run_daemon()
-}
--- src/lib.rs
+++
@@ -1,9 +0,0 @@
-// Author: Dustin Pilgrim
-// License: MIT
-
-pub mod cli;
-pub mod daemon;
-pub mod path;
-pub mod protocol;
-pub mod spec;
-pub mod wallpaper;
